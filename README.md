9778bf296d4163c56c414676b925b75b8f9bed64
galaxyonfire <40287291+galaxyonfire@users.noreply.github.com>
2020-04-02 05:24:33 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Offline Story Player for [chooseyourstory.com](chooseyourstory.com)
+
+A tool to download a story from [chooseyourstory.com](http://chooseyourstory.com) (CYS) to play offline
+
+By galaxyonfire on Github.com
+
+### Overview
+
+This a python script built using Python 3.7.6 and [MechanicalSoup](https://github.com/MechanicalSoup/MechanicalSoup) that searches through a story from CYS and rebuilds the story on your local device. You only need one piece of info to get the script running and that is the link to story you want to play.
+
+As an example, consider the story *Eternal* by Endmaster. The link for that story is [http://chooseyourstory.com/story/eternal](http://chooseyourstory.com/story/eternal).
+
+With this link in hand, if we look at line 8 of the script we'll see a `STORY_LINK` variable. Simply paste the link of the story your desire between the two single quotes and run the script to download the story offline.
+
+So for the above example line 8 should be `STORY_LINK = 'http://chooseyourstory.com/story/eternal'`
+
+### Installation
+
+The script requires your machine to have python 3.x, pip and a rudimentary understanding of the CLI. If you have no idea what those are that is ok, here are some resources to get started:
+* [Quick intro to CLI](https://www.w3schools.com/whatis/whatis_cli.asp)
+* Might I introduce you to [Anaconda](https://www.anaconda.com/distribution/) to help manage python if you've never programmed before.
+* [Window's](https://www.howtogeek.com/197947/how-to-install-python-on-windows/) specific and [macOS](https://docs.python-guide.org/starting/install3/osx/) specific guides if you want a more in-depth option
+
+Open your CLI in some directory and type `pip install mechanicalsoup` and hit enter to install MechanicalSoup if you haven't before. Note that if you are using Anaconda you must open the Anaconda Navigator, click on Environments, then on the play button next to base(root) to access python/pip through the CLI.
+
+Windows example:
+```
+C:\Users\You\Desktop> pip install mechanicalsoup
+...
+Successfully installed... mechanicalsoup
+```
+
+Or on Anaconda:
+```
+(base) C:\Users\You\Desktop> pip install mechanicalsoup
+...
+Successfully installed... mechanicalsoup
+```
+
+Afterwards download the story_download.py file and edit the `STORY_LINK` to your desired story.
+
+### Usage
+
+Open the CLI that has access to python in the directory of story_download.py. Now simply run `python story_download.py` and after a few minutes your story should be downloaded for offline viewing!
+
+In the same directory of the story_download.py file, there should be a "Play_the_story_title.html". Simply open that file with your desire web browser and enjoy!
+
+### Notes
+
+* Currently the script doesn't support games with inventory or images (i.e. it won't download images or save inventory).
+* Clicking 'Save' won't save your current progress locally but will instead tell you the current HTML file you are viewing in the Story_Data/the_story_title folder. Simply note that file and reopen it later to resume your progress.
---
+++ story_download.py
@@ -0,0 +1,176 @@
+import mechanicalsoup
+import requests
+import re
+import hashlib
+import os
+import sys
+
+STORY_LINK = ''
+
+#######################################################
+#
+#   Above is the only link you need to replace.
+#   Make sure the single quotes surround the link!
+#
+#   Below is the code -- feel free to take a look!
+#
+#######################################################
+
+assert(STORY_LINK != '')
+
+STORY = mechanicalsoup.StatefulBrowser()
+STORY.open(STORY_LINK)
+
+STORY_NAME = STORY.get_current_page().find(id='ctl09').string.strip()
+STORY_VIEWER_LINK = 'http://chooseyourstory.com/story/viewer/default.aspx?StoryId=' + re.findall(re.compile('\?[0-9]+'), STORY.get_current_page().find(type='submit')['onclick'])[0][1:]
+
+STORY.close()
+
+ONLY_NUM = re.compile('\d+')
+POSTBACK = re.compile('^P')
+ILLEGAL_CHARS = re.compile(':|\?')
+CSS_OBJECTS = ['/App_Themes/CYS/Styles.css', '/App_Themes/CYS/images/storyviewer_masthead_background.gif', '/App_Themes/CYS/images/storyviewer_masthead_logo.gif']
+
+def bhash(browser):
+    """
+        browser: a StatefulBrowser
+    
+    Returns the hash of the browser
+    based on story contents
+    """
+    return hashlib.sha1(browser.get_current_page().body.find(style="padding:0px 30px 50px 30px").div
+                        .encode('utf-8')).hexdigest()[:10]
+
+def pageTitle(browser):
+    """
+        browser: a StatefulBrowser
+
+    Returns a unique title of a page
+    """
+    page = browser.get_current_page()
+    title = page.find(id='_storyTitle').string
+    title = title.replace(" ", "_")
+    ftitle = re.sub(ILLEGAL_CHARS, '', title)
+    return ftitle + bhash(browser)
+
+def write(browser):
+    """
+        browser: a StatefulBrowser
+
+    Writes the HTML data of the page to
+    the corresponding .html file
+    """
+    name = pageTitle(browser) + ".html"
+    with open("Story_Data/" + STORY_NAME + '/' + name, "w") as f:
+        f.write(browser.get_current_page().prettify(formatter="html"))
+
+def getChildren(browser):
+    """
+        browser: a StatefulBrowser
+    
+    Generates a list of child browsers, where each child follows one of the
+    links/story options on the page of the parent browser.
+    """
+    children = []
+    for link in browser.links(onclick=POSTBACK):
+        if 'End Game and Leave Comments' == link.string:
+            return []
+        cp = mechanicalsoup.StatefulBrowser(session=browser.session)
+        cp.open(STORY_VIEWER_LINK)
+        cp.select_form('#pbForm')
+        cp['pbAction'] = 'FollowLink'
+        cp['pbValue'] = ONLY_NUM.findall(link['onclick'])[0]
+        cp.submit_selected()
+        children.append(cp)
+    return children
+
+def cleanup(browser, childTitles):
+    """
+        browser: a StatefulBrowser
+        childTitles: a list of unique pageTitles for each Postback link in the browser
+    
+    Removes server-dependence of a page and switches links to local files
+    Note that after cleanup you cannot call getChildren() successfully
+    """
+    links = browser.links
+    links[1]['href'] = START_NAME + '.html'
+    links[2]['href'] = "javascript: if (confirm('Your current page is " + pageTitle(browser) + ".html')) window.close();"
+
+    for index in range(4, len(links)):
+        # this is wonky but will have to do -- no other way to get child names w/hahes
+        if 'End Game and Leave Comments' == links[index].string:
+            links[index]['href'] = 'http://chooseyourstory.com'
+        else:
+            fstring = childTitles[index-4]
+            links[index]['href'] = fstring + ".html"
+        del links[index]['onclick']
+
+    browser.get_current_page().form.decompose()
+    browser.get_current_page().script.decompose()
+    browser.get_current_page().link['href'] = "../css/Styles.css"
+    temp = str(browser.get_current_page().style.string)
+    temp = temp.replace('/App_Themes/CYS/images/storyviewer_masthead_background.gif', '../css/storyviewer_masthead_background.gif')
+    browser.get_current_page().style.string.replace_with(temp)
+    browser.get_current_page().img['src'] = '../css/storyviewer_masthead_logo.gif'
+
+def chartstory(start_browser):
+    """
+        start_browser: the StatefulBrowser of the 'first page' of the story
+    
+    Chart the story in a BFS inspired manner and
+    clean/write each page to a HTML file
+    """
+
+    fringe = []
+    fringe.append(start_browser)
+    visited = set()
+    visited.add(pageTitle(start_browser))
+    
+    while(True):
+        if len(fringe) == 0:
+            break
+        
+        node = fringe.pop(0)
+        visited.add(pageTitle(node))
+        
+        childTitles = []
+        for child in getChildren(node):
+            childTitles.append(pageTitle(child))
+            if pageTitle(child) not in visited:
+                fringe.append(child)
+            else:
+                child.close()
+        cleanup(node, childTitles)
+        write(node)
+        node.close()
+
+if __name__ == '__main__':
+    start_browser = mechanicalsoup.StatefulBrowser()
+    start_browser.open(STORY_VIEWER_LINK)
+
+    if 'Story_Data' not in os.listdir():
+        os.mkdir('Story_Data')
+
+    if 'css' not in os.listdir('Story_Data/'):
+        os.mkdir('Story_Data/css')
+        for obj in CSS_OBJECTS:
+            url = 'http://chooseyourstory.com' + obj
+            name = obj.split('/')[-1]
+            r = requests.get(url)
+            with open('Story_Data/css/' + name, 'wb') as f:
+                f.write(r.content)
+
+    if STORY_NAME not in os.listdir('Story_Data/'):
+        os.mkdir('Story_Data/' + STORY_NAME)
+    else:
+        print("ALERT: " + STORY_NAME + " seems to already be downloaded, rerunning this script will overwrite current files")
+        print("Proceed? ", end='')
+        if input('[yes/no] ') != 'yes':
+            sys.exit()
+
+    start_html = '<html><meta http-equiv="refresh" content="0; Story_Data/' + STORY_NAME + '/' + pageTitle(start_browser) + '.html' + '"><meta name="keywords" content="automatic redirection"></html>'
+
+    with open('Play_' + STORY_NAME + '.html', 'w') as f:
+        f.write(start_html)
+
+    chartstory(start_browser)
